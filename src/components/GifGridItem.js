3225101c4f77944b9e1966d49ff3588252714e9d
Ignacio <dev-ignacio-h@gmail.com>
2021-02-23 23:15:23 -0300

    tests in GifGridItem component

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,16 +1,18 @@
-import 'animate.css/animate.min.css'
+import 'animate.css/animate.min.css';
+import PropTypes from 'prop-types';
 
-const GifGridItem = ({id, title, url}) => {
-    // console.log(img);
-    // console.log(id)
-    // console.log(title)
-    // console.log(url)
-    return (
-        <div className="card animate__animated animate__bounce">
-            <img src={url} alt={title} />
-            <p>{title}</p>
-        </div>
-    );
+const GifGridItem = ({ title, url }) => {
+  return (
+    <div className="card animate__animated animate__bounce">
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
+  );
+};
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
- 
+
 export default GifGridItem;
